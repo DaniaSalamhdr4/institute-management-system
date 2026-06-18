33b045b31d997a1cad732a4c01336e434f8a70fd
Fadia23 <fadiahk24@gmail.com>
2026-06-18 15:53:02 +0300

    "Initial commit for users feature"

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+export class CreateUserDto {
+    name!: string;
+    email!: string;
+    password!: string;
+    phone!: string;
+    registration_year!: number;
+    specialization!: string;
+    role!: string;
+}
---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,12 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    name!: string;
+    email!: string;
+    password!: string;
+    phone!: string;
+    registration_year!: number;
+    specialization!: string;
+    role!: string;
+}
