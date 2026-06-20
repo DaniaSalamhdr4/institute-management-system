e1463dd0e6ca81faa8e8e4d1ff3ab6a85313922a
demaramadan <dema.rm2000@gmail.com>
2026-06-20 13:19:21 +0000

    Jun 20, 2026, 2:19 PM

--- src/enrollments/enrollments.service.ts
+++ src/enrollments/enrollments.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { Enrollment } from './schemas/enrollments.schema';
@@ -12,7 +16,7 @@ export class EnrollmentsService {
   ) {}
 
   //Create New Student
-    async create(createEnrollmentDto: CreateEnrollmentDto) {
+  async create(createEnrollmentDto: CreateEnrollmentDto): Promise<Enrollment> {
     const { student_id, course_id, grade } = createEnrollmentDto;
 
     const existing = await this.enrollmentModel.findOne({
@@ -28,12 +32,11 @@ export class EnrollmentsService {
     const newEnrollment = new this.enrollmentModel({
       student_id: new Types.ObjectId(student_id),
       course_id: new Types.ObjectId(course_id),
-      grade: grade !== undefined ? grade : null, 
+      grade: grade !== undefined ? grade : null,
     });
 
     return await newEnrollment.save();
   }
-
 
   //Get All Student
   async findAll() {
@@ -41,39 +44,39 @@ export class EnrollmentsService {
       return await this.enrollmentModel
         .find()
         // .populate({ path: 'student_id', select: 'name email', strictPopulate: false })
-        // .populate({ path: 'course_id', select: 'course_name passing_grade', strictPopulate: false }) 
+        // .populate({ path: 'course_id', select: 'course_name passing_grade', strictPopulate: false })
         .exec();
     } catch (error) {
       console.error('Error Get All :', (error as any).message);
-      return []; 
+      return [];
     }
   }
 
-    // 2. جلب جميع التسجيلات وعرض كافة المعلومات المتعلقة بالطلاب والدورات بأمان
+  // 2. جلب جميع التسجيلات وعرض كافة المعلومات المتعلقة بالطلاب والدورات بأمان
   // async findAll() {
   //   try {
   //     const data = await this.enrollmentModel
   //       .find()
   //       .populate({ path: 'student_id', select: 'name email', strictPopulate: false })
-  //       .populate({ path: 'course_id', select: 'course_name passing_grade', strictPopulate: false }) 
+  //       .populate({ path: 'course_id', select: 'course_name passing_grade', strictPopulate: false })
   //       .exec();
 
   //     // نقوم بعمل فلترة لإرجاع التسجيلات التي تحتوي على كائنات سليمة فقط وتخطي البيانات القديمة التالفة
   //     return data.filter(item => item && item.student_id && item.course_id);
   //   } catch (error) {
   //     console.error('Error Get All :', (error as any).message);
-  //     return []; 
+  //     return [];
   //   }
   // }
 
-
-
   // Get All Courses For One Student
   async findByStudent(studentId: string) {
-   return  await this.enrollmentModel.find({
-    student_id : new Types.ObjectId(studentId)})
-    // .populate('course_id','course_name passing_grade')
-     .exec();
+    return await this.enrollmentModel
+      .find({
+        student_id: new Types.ObjectId(studentId),
+      })
+      // .populate('course_id','course_name passing_grade')
+      .exec();
   }
 
   // Delete student registration in course
@@ -87,15 +90,13 @@ export class EnrollmentsService {
   // Update Grade
   async updateGrade(id: string, updateGradeDto: UpdateEnrollmentDto) {
     const updated = await this.enrollmentModel
-      .findByIdAndUpdate(
-        id,
-        { grade: updateGradeDto.grade },
-        { new: true },
-      )
+      .findByIdAndUpdate(id, { grade: updateGradeDto.grade }, { new: true })
       .exec();
 
     if (!updated) {
-      throw new NotFoundException('Registration dose not exist to modify Grade .');
+      throw new NotFoundException(
+        'Registration dose not exist to modify Grade .',
+      );
     }
 
     return { message: 'Grade has been successfully updated .', data: updated };
--- src/enrollments/schemas/enrollments.schema.ts
+++ src/enrollments/schemas/enrollments.schema.ts
@@ -1,19 +1,23 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+// import { User } from '../../users/schema/user.schema';
 
 @Schema({ collection: 'enrollments', timestamps: true })
 export class Enrollment extends Document {
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  student_id?: Types.ObjectId;
 
-  @Prop({type : Types.ObjectId , ref : 'User' , required :true})
-  student_id?: Types.ObjectId ;
-
-  @Prop({type : Types.ObjectId , ref : 'Course' , required :true})
+  @Prop({ type: Types.ObjectId, ref: 'Course', required: true })
   course_id?: Types.ObjectId;
 
+  // @Prop({ type: Types.ObjectId, ref: User.name, required: true })
+  // student_id?: Types.ObjectId;
+
+  // @Prop({ type: Types.ObjectId, ref: 'Course', required: true }) // تأكد أن زميلك سمى الكلاس Course
+  // course_id?: Types.ObjectId;
 
   @Prop({ type: Number })
-  grade?: number ; 
+  grade?: number;
 }
-
 
 export const EnrollmentSchema = SchemaFactory.createForClass(Enrollment);
