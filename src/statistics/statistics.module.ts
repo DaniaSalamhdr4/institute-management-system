3d6aeaefe99477cfbcf287a4a7d8849621723133
demaramadan <dema.rm2000@gmail.com>
2026-06-21 21:17:59 +0000

    Jun 21, 2026, 10:17 PM

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -7,8 +7,10 @@ import { Course } from '../courses/schema/course.schema';
 import { EnrollmentSchema } from '../enrollments/schema/enrollments.schema';
 import { CourseSchema } from '../courses/schema/course.schema';
 import { User, UserSchema } from '../users/schema/user.schema';
+import { AuthModule } from '../auth/auth.module';
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([
       {
         name: Enrollment.name,
